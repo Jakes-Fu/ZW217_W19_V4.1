9bc1182341b97d2d3e3f5b959ca5b754406f32e6
fys <1424004358@qq.com>
2024-07-04 12:40:54 +0800

    init

---
+++ MS_MMI_Main/source/mmi_app/app/zmt_hanzi/h/zmt_hanzi_mdu_def.h
@@ -0,0 +1,87 @@
+
+//[[define image information
+
+//]]define image information
+
+
+
+//[[define anim information
+
+//]]define anim information
+
+
+//[[define string information
+
+RES_ADD_STRING(HANZI_CARD,"汉字学习")
+RES_ADD_STRING(HANZI_PACTISE,"汉字学习")
+RES_ADD_STRING(HANZI_NEW_WORD,"生词本")
+RES_ADD_STRING(HANZI_MASTERED,"已掌握")
+RES_ADD_STRING(HANZI_UNMASTERED,"未掌握")
+RES_ADD_STRING(HANZI_NEXT_CHAPTER,"下一单元")
+RES_ADD_STRING(HANZI_BACK_CHAPTER,"返回")
+RES_ADD_STRING(HANZI_RENJIAO_EDITION,"人教版")
+RES_ADD_STRING(HANZI_SIMILAR_WORD,"【形近字】")
+RES_ADD_STRING(HANZI_NO_UNMASTER,"本单元暂无未掌握的汉字")
+//]]define string information
+
+//[[define image information
+
+	/*RES_ADD_IMG(ZMT_LISTEN_ALBUM,"\\MMI_RES_##\\IMAG\\dsl_listen_album.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_DOWNLOAD,"\\MMI_RES_##\\IMAG\\dsl_listen_download.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_DOWNLOADING,"\\MMI_RES_##\\IMAG\\dsl_listen_downloading.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_SELECT,"\\MMI_RES_##\\IMAG\\dsl_listen_select.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_UNSELECT,"\\MMI_RES_##\\IMAG\\dsl_listen_unselect.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_LOOP,"\\MMI_RES_##\\IMAG\\dsl_listen_loop.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_RANDOM,"\\MMI_RES_##\\IMAG\\dsl_listen_random.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_SINGLE,"\\MMI_RES_##\\IMAG\\dsl_listen_single.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_NEXT,"\\MMI_RES_##\\IMAG\\dsl_listen_next.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_PRE,"\\MMI_RES_##\\IMAG\\dsl_listen_pre.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_PAUSE,"\\MMI_RES_##\\IMAG\\dsl_listen_pause.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_PLAY,"\\MMI_RES_##\\IMAG\\dsl_listen_play.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_SECLET_ALL,"\\MMI_RES_##\\IMAG\\dsl_listen_all.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_BACK,"\\MMI_RES_##\\IMAG\\dsl_listen_back.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_DELECT,"\\MMI_RES_##\\IMAG\\dsl_listen_delect.png",IMG_CMP_PNG_ARGB_RLE,0)
+	RES_ADD_IMG(ZMT_LISTEN_LOADING,"\\MMI_RES_##\\IMAG\\dsl_listen_loading.png",IMG_CMP_PNG_ARGB_RLE,0)
+*/	
+RES_ADD_IMG(ZMT_LISTEN_VOLUME,"\\MMI_RES_##\\IMAG\\dsl_listen_volume.png",IMG_CMP_PNG_ARGB_RLE,0)
+
+//]]define image information
+
+
+
+//[[define anim information
+
+
+//]]define anim information
+
+
+//[[define string information
+	RES_ADD_STRING(ZMT_TXT_LISTENING, "分级听力")
+	RES_ADD_STRING(ZMT_TXT_LOCAL, "本地")
+	RES_ADD_STRING(ZMT_TXT_NO_LOCAL, "暂无本地资源")
+	RES_ADD_STRING(ZMT_TXT_DOWNLOADED, "已下载")
+	RES_ADD_STRING(ZMT_TXT_LOADING, "加载中")
+	RES_ADD_STRING(ZMT_TXT_LOADING_FAIL, "加载失败")
+	RES_ADD_STRING(ZMT_TXT_NO_AUDIO_DATA, "暂无音频数据")
+	RES_ADD_STRING(ZMT_TXT_DOWNLOAD_TIPS, "全部下载需要耗费一定的流量，点击确认后下载")
+	RES_ADD_STRING(ZMT_TXT_DOWNLOAD_OK, "确认")
+	RES_ADD_STRING(ZMT_TXT_DOWNLOAD_CANCEL, "取消")
+	RES_ADD_STRING(ZMT_TXT_PRE_SCHOOL, "学前")
+	RES_ADD_STRING(ZMT_TXT_PRIMARY_SCHOOL, "小学")
+	RES_ADD_STRING(ZMT_TXT_JUNIOR_SCHOOL, "初中")
+	RES_ADD_STRING(ZMT_TXT_LYRICS, "字幕")
+	RES_ADD_STRING(ZMT_TXT_LOOP_PLAY, "循环")
+	RES_ADD_STRING(ZMT_TXT_SINGLE_PLAY, "单曲")
+	RES_ADD_STRING(ZMT_TXT_RANDOM_PLAY, "随机")
+
+	RES_ADD_STRING(ZMT_TXT_TF_NO_SPACE, "TF卡内存不足")
+	RES_ADD_STRING(ZMT_TXT_NO_NEXT, "当前列表无下一首")
+	RES_ADD_STRING(ZMT_TXT_NO_PRE, "当前列表无上一首")
+	RES_ADD_STRING(ZMT_TXT_DOWNLOAD_WAITTING, "正在下载，请稍后")
+	RES_ADD_STRING(ZMT_TXT_DOWNLOAD_FAIL, "下载失败，请重试")
+	
+//]]define string information
+
+
+
+//#endif
